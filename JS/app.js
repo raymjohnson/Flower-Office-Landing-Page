67e957c6b59f54df565d1d1329ba2f39814116ea
Rachel Johnson <r.mariejohnson@hotmail.com>
2017-08-11 12:08:44 +1000

    new function

--- JS/app.js
+++ JS/app.js
@@ -1,77 +1,111 @@
 $(document).ready(function() {
 
-    // SVG Timer Function
-    function flowerPower() {
+    animation_order = [
+        ".layer_1",
+        ".layer_2",
+        ".layer_3",
+        ".layer_4",
+        ".layer_2",
+        ".layer_5",
+        ".layer_6",
+        ".layer_4"
+    ];
+    no_animations = animation_order.length;
+    animation_delay = 500;
 
-        window.setTimeout(function(){
-            $('.layer_1').show();            
-        }, 1000);
+    function change_display(to_hide, to_show){
+        $(to_hide).hide();
+        $(to_show).show();
+    }
 
-        window.setTimeout(function(){
-            $('.layer_1').hide();
-            $('.layer_2').show();            
-        }, 1500);
+    // i (arbitrary) = increment (start increment at 0, i is < the no. of animations)
+    for(i = 0; i < no_animations - 1; i++){
+        to_hide = animation_order[i];
+        to_show = animation_order[i+1];
 
-        window.setTimeout(function(){
-            $('.layer_2').hide();
-            $('.layer_3').show();            
-        }, 2000);
-
-        window.setTimeout(function(){
-            $('.layer_3').hide();
-            $('.layer_4').show();            
-        }, 2500);
-
-        window.setTimeout(function(){
-            $('.layer_4').hide();
-            $('.layer_2').show();            
-        }, 3000);
-
-        window.setTimeout(function(){
-            $('.layer_2').hide();
-            $('.layer_5').show();            
-        }, 3500);
-
-        window.setTimeout(function(){
-            $('.layer_5').hide();
-            $('.layer_6').show();            
-        }, 4000);
-
-        window.setTimeout(function(){
-            $('.layer_6').hide();
-        }, 4500);
-
-        window.setTimeout(function(){
-            $('.layer_6').show();            
-        }, 5000);
-
-        window.setTimeout(function(){
-            $('.layer_6').hide();
-        }, 5500);
-
-        window.setTimeout(function(){
-
-            $('.layer_6').show();            
-        }, 6000);
-
-        window.setTimeout(function(){
-            $('.layer_6').hide();
-        }, 6500);
-
-        window.setTimeout(function(){
-            $('.layer_6').show();            
-        }, 7000);
-
-        window.setTimeout(function(){
-            $('.layer_6').hide();
-        }, 7500);
-
-        window.setTimeout(function(){
-            $('.layer_4').show();            
-        }, 8000);
-    } 
-
-    // Calls
-    flowerPower();    
-
+        setTimeout(change_display(to_hide,to_show), animation_delay * (i+1));
+    }
 });
+
+
+
+
+
+
+
+
+
+
+    // // SVG Timer Function - OLD
+    // function flowerPower() {
+
+    //     window.setTimeout(function(){
+    //         $('.layer_1').show();            
+    //     }, 1000);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_1').hide();
+    //         $('.layer_2').show();            
+    //     }, 1500);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_2').hide();
+    //         $('.layer_3').show();            
+    //     }, 2000);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_3').hide();
+    //         $('.layer_4').show();            
+    //     }, 2500);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_4').hide();
+    //         $('.layer_2').show();            
+    //     }, 3000);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_2').hide();
+    //         $('.layer_5').show();            
+    //     }, 3500);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_5').hide();
+    //         $('.layer_6').show();            
+    //     }, 4000);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_6').hide();
+    //     }, 4500);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_6').show();            
+    //     }, 5000);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_6').hide();
+    //     }, 5500);
+
+    //     window.setTimeout(function(){
+
+    //         $('.layer_6').show();            
+    //     }, 6000);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_6').hide();
+    //     }, 6500);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_6').show();            
+    //     }, 7000);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_6').hide();
+    //     }, 7500);
+
+    //     window.setTimeout(function(){
+    //         $('.layer_4').show();            
+    //     }, 8000);
+    // } 
+
+    // // Calls
+    // flowerPower();    
--- README.md
+++
@@ -1,3 +0,0 @@
-# Flower-Office-Landing-Page
-Hold page for Flower Office store. Loading animation created using SVGs and jQuery.
-http://www.floweroffice.com.au/
